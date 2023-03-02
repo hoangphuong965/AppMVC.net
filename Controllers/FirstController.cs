8bf20c3b40f2176970649e7eb06f0c0ab8c585fb
Phuong Hoang <you@example.com>
2023-03-02 19:53:55 +0700

    create MVC

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,106 @@
+﻿using AppMvc.Net.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Linq;
+
+namespace AppMvc.Net.Controllers
+{
+    public class FirstController : Controller
+    {
+        private readonly ILogger _logger;
+        private readonly ProductService _productService;
+
+        public FirstController(ILogger<FirstController> logger, ProductService productService) 
+        {
+            _logger= logger;
+            _productService= productService;    
+        }
+
+        public string Index()
+        {
+            // this.HttpContext
+            // this.Request
+            // this.Response
+            // this.RouteData
+
+            // this.User
+            // this.ModelState
+            // this.ViewData
+            // this.ViewBag
+            // this.Url
+            // this TempData
+
+            _logger.LogInformation("index action");
+            _logger.LogWarning("");
+            _logger.LogInformation("");
+            return "Toi la FirstController index";
+        }
+
+        public void Nothing()
+        {
+            _logger.LogInformation("Nothing Action");
+            Response.Headers.Add("hi", "Xin chao cac ban");
+        }
+
+        public IActionResult Brid()
+        {
+            string FilePath = Path.Combine(Startup.ContentRootPath, "FIles", "b.jpg");
+            var bytes = System.IO.File.ReadAllBytes(FilePath);
+
+            return File(bytes, "image/jpg");
+        }
+        
+        public IActionResult IphonePrice()
+        {
+            return Json(
+                new
+                {
+                    productName = "Iphone X",
+                    Price = 1000 
+                }
+            );
+        }
+
+        public IActionResult Privacy()
+        {
+            var url = Url.Action("Privacy", "Home");
+            return LocalRedirect(url);
+        }
+
+        public IActionResult Google()
+        {
+            var url = "https://www.google.com/";
+            return Redirect(url);
+        }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public IActionResult ViewProduct(int? id)
+        {
+            var product = _productService.Where(product => product.Id == id).FirstOrDefault();
+            if (product == null)
+            {
+                //TempData["StatusMessage"] = "San pham ban yeu cau khong co";
+                StatusMessage = "San pham ban yeu cau khong co";
+                return Redirect(Url.Action("Index", "Home"));
+            }
+
+            // cách 1: truyền dữ liệu sang view bằng cách sử dụng model: thiết lập model cho view
+            //return View(product);
+
+            // cách 2: dùng viewdata
+            //ViewData["product"] = product;
+            //ViewData["Title"] = product.Name;
+            //return View("ViewProduct2");
+
+            //TempData["Thongbao"] = ""; cai nay su dung session
+
+            ViewBag.Product = product;
+            return View("ViewProduct3");
+        }
+    }
+
+    
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,19 @@
+﻿using AppMvc.Net.Models;
+using System.Collections.Generic;
+
+namespace AppMvc.Net.Services
+{
+    public class ProductService : List<ProductModel>
+    {
+        public ProductService() 
+        {
+            this.AddRange(new List<ProductModel>
+            {
+                new ProductModel() {Id = 1, Name = "IPhone", Price = 1000},
+                new ProductModel() {Id = 2, Name = "Samsung", Price = 1200},
+                new ProductModel() {Id = 3, Name = "Sony", Price = 890},
+                new ProductModel() {Id = 4, Name = "Nokia", Price = 200},
+            });
+        }
+    }
+}
